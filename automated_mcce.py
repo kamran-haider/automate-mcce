648181f190fc7bc990683efbb0299e07e00d5910
kamran <kamranhaider.mb@gmail.com>
2016-06-14 10:29:26 -0400

    First steps towards a consolidated automated mcce program.

---
+++ automated_mcce.py
@@ -0,0 +1,25 @@
+#!/usr/local/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+Automate MCCE Calculations.
+Description
+-----------
+This module implements a program to set up and run automated mcce calculations.
+Notes
+-----
+This is still in development.
+References
+----------
+Examples
+--------
+Coming soon!
+
+TODO
+----
+	* Envision batch mcce runs.
+	* Do we always start from a folder containing PDB files or are there other ways too?
+	* Collect all current utility functions.
+	*
+"""
