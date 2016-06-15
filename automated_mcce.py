00823ca7f5d14939e9e6bcc9f0ec36eb25a96e31
kamran <kamranhaider.mb@gmail.com>
2016-06-15 11:10:42 -0400

    setting up new automation script.

--- automated_mcce.py
+++ automated_mcce.py
@@ -1,5 +1,7 @@
 #!/usr/local/bin/env python
 # -*- coding: utf-8 -*-
+import sys
+from optparse import OptionParser
 
 
 """
@@ -23,3 +25,35 @@ TODO
 	* Collect all current utility functions.
 	*
 """
+
+def automated_run(input_dir, destination_dir, mcce_dir):
+	# check if all inout_dir exists and is not empty
+	# check if mcce exists and is executable
+	# if outout dir doesnot exist, create it 
+	pass
+
+
+
+
+
+def main():
+    parser = OptionParser()
+    parser.add_option("-i", "--input_directory", dest="input_directory", type="string", help="Directory containing input PDB files.")
+    parser.add_option("-d", "--destination_directory", dest="destination_directory", type="string", help="Directory where output of mcce calculations will be stored.")
+    parser.add_option("-e", "--mcce_directory", dest="mcce_directory", type="string", help="Ligand file")
+    (options, args) = parser.parse_args()
+    if len(args) == 0:
+        print "no argument given!"
+        parser.print_help()    
+    else:
+        print "Setting up calculations..."
+    	automated_run(options.input_directory, options.destination_directory, options.mcce_directory)
+
+# Using entry point approach for future conda packaging
+def entry_point():
+    main()
+    
+if __name__ == '__main__':
+    entry_point()
+
+
